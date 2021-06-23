800e18f28e77732ba067face9365fe2170325c4a
AdonaiTim <wolextim@gmail.com>
2021-06-23 16:53:18 +0100

    updates

--- lib/models/Bank.dart
+++ lib/models/Bank.dart
@@ -1,14 +1,14 @@
 class Bank {
   int id;
   String bank;
-  Null createdAt;
-  Null updatedAt;
+  String createdAt;
+  String updatedAt;
 
   Bank({this.id, this.bank, this.createdAt, this.updatedAt});
 
   Bank.fromJson(Map<String, dynamic> json) {
     id = json['id'];
-    bank = json['bank'];
+    bank = json['bank_name'];
     createdAt = json['created_at'];
     updatedAt = json['updated_at'];
   }
@@ -16,7 +16,7 @@ class Bank {
   Map<String, dynamic> toJson() {
     final Map<String, dynamic> data = new Map<String, dynamic>();
     data['id'] = this.id;
-    data['bank'] = this.bank;
+    data['bank_name'] = this.bank;
     data['created_at'] = this.createdAt;
     data['updated_at'] = this.updatedAt;
     return data;
