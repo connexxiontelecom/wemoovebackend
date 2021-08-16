1cb9a0dc4dde91f908234d82b42f5b4c1597628c
AdonaiTim <wolextim@gmail.com>
2021-08-16 16:04:21 +0100

    updates

--- app/Http/Controllers/walletsController.php
+++ app/Http/Controllers/walletsController.php
@@ -323,8 +323,64 @@ class walletsController extends Controller
         $this->credit($amountPaid,$account->user_id, "Monnify");
         //return response()->json(compact("account"));
         //return response()->json(compact("clientSecret",'amountPaid',"transactionReference", 'paymentReference', 'transactionhash', 'isequal', 'accountRef', 'paymentMethod', 'paymentStatus' ));
+        $this->ToSpecificUser("Credit Alert", "Your wallet has been Credited with $amountPaid", $account->user_id);
     }
 
+
+
+    public function pushtoToken($token, $title, $body, $userId)
+    {
+        //$token, $title, $body, $userId, $tenantId
+
+        $ch = curl_init("https://fcm.googleapis.com/fcm/send");
+
+        $data = array("clickaction" => "FLUTTERNOTIFICATIONCLICK", "user" => $userId);
+
+        //Creating the notification array.
+        $notification = array('title' => $title, 'body' => $body);
+
+        //This array contains, the token and the notification. The 'to' attribute stores the token.
+        $arrayToSend = array('to' => $token, 'notification' => $notification, 'data' => $data);
+
+        //Generating JSON encoded string form the above array.
+        $json = json_encode($arrayToSend);
+
+        $url = "https://fcm.googleapis.com/fcm/send";
+        //Setup headers:
+        $headers = array();
+        $headers[] = 'Content-Type: application/json';
+        $headers[] = 'Authorization: key=AAAAlftCod0:APA91bH_f5ZouSisahhGVjPDS_fbx54iR1noZeeI9gGN7b6KCTBamaDebRKN4zQlftXWLVKgv2zOP2vgg1NaP9W1Qbc1_8BinzGkE8J8pvL19jEE2HvaMqQBz2MKi2uuot4laQZH7EHt';
+
+        //Setup curl, add headers and post parameters.
+        curl_setopt($ch, CURLOPT_CUSTOMREQUEST, "POST");
+        curl_setopt($ch, CURLOPT_POSTFIELDS, $json);
+        curl_setopt($ch, CURLOPT_HTTPHEADER, $headers);
+
+        curl_setopt($ch, CURLOPT_URL, $url);
+        curl_setopt($ch, CURLOPT_POST, true);
+        curl_setopt($ch, CURLOPT_HTTPHEADER, $headers);
+        curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+        curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, false);
+        //$result = curl_exec($ch);
+        // print($result);
+
+        //Send the request
+        curl_exec($ch);
+
+        //Close request
+        curl_close($ch);
+    }
+
+    public function ToSpecificUser($title, $body, $userId)
+    {
+        $users = User::where('users.id', $userId)->orderBy('id', 'DESC')->get();
+        foreach ($users as $user) {
+            $token = $user['device_token'];
+            if ($token != null && !empty($token)) {
+                $this->pushtoToken($token, $title, $body, $userId);
+            }
+        }
+    }
 
 
 }
